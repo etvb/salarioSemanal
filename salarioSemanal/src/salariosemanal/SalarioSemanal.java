6505f51a530265679ffa6f0c3b26e83e6d0ffa64
Edgardo Torres Vanden Bouch <etvb66@hotmail.com>
2017-08-17 19:08:26 -0500

    quitamos lo que no se usa

--- salarioSemanal/src/salariosemanal/SalarioSemanal.java
+++ salarioSemanal/src/salariosemanal/SalarioSemanal.java
@@ -1,27 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package salariosemanal;
 
 import java.util.Scanner;
 
-/**
- *
- * @author edgardo
- */
 public class SalarioSemanal {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
        Scanner entrada = new Scanner(System.in);
         int horasTrabajadas;
         final float salarioXHora = 55.80f;
-        float salarioDiario;
         /*
         a la semana se trabajan 45 horas semanales
         */
